6542f16e6d6503da6b89c125a6ccd59492eb4e4a
ddhsl <dnjs9199@naver.com>
2024-11-28 01:08:25 +0900

    Update

---
+++ daily_react/react/styled-components-pr/src/App.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+const SimpleButton = styled.button`
+color:white;`;
+
+function App() {
+  return (
+    <div>
+      <SimpleButton>ㅇㅇ</SimpleButton>
+    </div>
+  );
+}
+export default App;
+
